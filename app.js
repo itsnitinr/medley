2776aea69980074da4070a972dfdc4609c1079bb
Nitin Ranganath <nitinranganath@gmail.com>
2020-02-11 21:58:02 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,170 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const session = require("express-session");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Enter a long string here",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://admin-nitinr:mumbai97@medleydb-elarv.mongodb.net/medleyDB", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    googleId: String,
+    lyrics: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get("/auth/google/secrets", 
+passport.authenticate('google', { failureRedirect: '/login' }),
+function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/home");
+});
+
+app.get("/home", function(req, res) {
+    
+    if (req.isAuthenticated()) {
+        User.find({"lyrics": {$ne: null}}, function(err, foundUsers) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUsers) {
+                    res.render("main", {foundUsers: foundUsers});
+                }
+            }
+        });
+    } else {
+        res.redirect("/login");
+    }
+
+});
+
+app.post("/register", function(req, res) {
+    
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local") (req, res, function() {
+                res.redirect("/home");
+            });
+        }
+    });
+
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", function(req, res) {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local") (req, res, function() {
+                res.redirect("/home");
+            });
+        }
+    });
+
+});
+
+app.get("/submit", function(req, res) {
+    
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+
+});
+
+app.post("/submit", function(req, res) {
+
+    const submittedLyric = req.body.lyrics;
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                foundUser.lyrics = submittedLyric;
+                foundUser.save(function() {
+                    res.redirect("/home");
+                });
+            }
+        }
+    });
+
+});
+
+app.listen(3000, function(){
+    console.log("Sever running on port 3000");
+});
